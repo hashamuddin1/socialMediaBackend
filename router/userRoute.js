5d4196c3d84cc683ce286e162a9ea695f0273a64
hashamuddin1 <hashamlaptop@gmail.com>
2024-03-20 05:12:26 +0500

    create route of get profile

--- router/userRoute.js
+++ router/userRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const userRouter = express.Router();
 
-const { userSignUp, userLogin } = require("../controllers/userController");
+const {
+  userSignUp,
+  userLogin,
+  getProfile,
+} = require("../controllers/userController");
 
 userRouter.post("/api/userSignUp", userSignUp);
 userRouter.post("/api/userLogin", userLogin);
+userRouter.get("/api/getProfile", getProfile);
 
 module.exports = userRouter;
