4519c9b4619246e9f38f8b82edbbc7406d5f9403
hashamuddin1 <hashamlaptop@gmail.com>
2024-03-29 04:42:50 +0500

    create route of get posts by user

--- router/postRoute.js
+++ router/postRoute.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const postRouter = express.Router();
-const { addPost } = require("../controllers/postController");
+const { addPost, getMyPost } = require("../controllers/postController");
 const { verifyToken } = require("../middleware/auth");
 
 postRouter.post("/api/addPost", [verifyToken], addPost);
+postRouter.post("/api/getMyPost", [verifyToken], getMyPost);
 
 module.exports = postRouter;
