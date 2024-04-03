58f66a81dce9f9dfe1c88e6c2279d3992186e942
hashamuddin1 <hashamlaptop@gmail.com>
2024-04-04 01:38:40 +0500

    Create Route of Update Post

--- router/postRoute.js
+++ router/postRoute.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const postRouter = express.Router();
-const { addPost, getMyPost } = require("../controllers/postController");
+const {
+  addPost,
+  getMyPost,
+  updateMyPost,
+} = require("../controllers/postController");
 const { verifyToken } = require("../middleware/auth");
 
 postRouter.post("/api/addPost", [verifyToken], addPost);
 postRouter.post("/api/getMyPost", [verifyToken], getMyPost);
+postRouter.post("/api/updatePost", [verifyToken], updateMyPost);
 
 module.exports = postRouter;
